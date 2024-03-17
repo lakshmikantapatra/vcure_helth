f7e4eb7c7c937cd53e0180f635cb7371aea79db8
Lakshmikanta Patra <mr.lakshmikanta.patra@gmail.com>
2024-03-17 15:24:10 +0530

    define `db` and `UPLOAD_PATH` constant

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -43,6 +43,8 @@ abstract class BaseController extends Controller
      */
     // protected $session;
 
+    protected $db;
+
     /**
      * @return void
      */
@@ -54,5 +56,6 @@ abstract class BaseController extends Controller
         // Preload any models, libraries, etc, here.
 
         // E.g.: $this->session = \Config\Services::session();
+        $this->db = \Config\Database::connect();
     }
 }
